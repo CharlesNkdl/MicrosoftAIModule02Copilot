cfae1d11cc897513e24c1c0a7ef4aaf49f4d76c3
CharlesNkdl <100453363+CharlesNkdl@users.noreply.github.com>
2023-07-19 21:14:03 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,41 +1,51 @@
-// create web server
-// 1. load modules
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-// 2. create web server
-var server = http.createServer(function(request,response){
-    var parsedUrl = url.parse(request.url);
-    var resource = parsedUrl.pathname;
-    console.log('resource='+resource);
+// Create web server
+var express = require('express');
+var router = express.Router();
+var Comments = require('../models/comments');
 
-    // 3. process request
-    if(resource == '/'){
-        fs.readFile('index.html','utf-8',function(error,data){
-            if(error){
-                response.writeHead(500,{'Content-Type':'text/html'});
-                response.end('500 Internal Server '+error);
-            }else{
-                response.writeHead(200,{'Content-Type':'text/html'});
-                response.end(data);
-            }
-        });
-    }else if(resource == '/comments'){
-        if(request.method == 'POST'){
-            request.on('data',function(data){
-                var parsedStr = qs.parse(data.toString());
-                var comment = parsedStr.comment;
-                console.log('comment='+comment);
-                response.writeHead(200,{'Content-Type':'text/html'});
-                response.end(comment);
-            });
-        }else{
-            response.writeHead(200,{'Content-Type':'text/html'});
-            response.end('GET method not supported.');
-        }
-    }else{
-        response.writeHead(404,{'Content-Type':'text/html'});
-        response.end('404 Page Not Found.');
+// Get all comments
+router.get('/', function(req, res) {
+  Comments.find({}, function(err, comments) {
+    if (err) {
+      res.status(500).send('Error occurred: database error');
+    } else {
+      res.send(comments);
     }
+  });
 });
+
+// Get comments for a specific user
+router.get('/:user', function(req, res) {
+  Comments.find({user: req.params.user}, function(err, comments) {
+    if (err) {
+      res.status(500).send('Error occurred: database error');
+    } else {
+      res.send(comments);
+    }
+  });
+});
+
+// Add a comment
+router.post('/', function(req, res) {
+  var comment = new Comments(req.body);
+  comment.save(function(err) {
+    if (err) {
+      res.status(500).send('Error occurred: database error');
+    } else {
+      res.send(comment);
+    }
+  });
+});
+
+// Delete a comment
+router.delete('/:id', function(req, res) {
+  Comments.findByIdAndRemove(req.params.id, function(err, comment) {
+    if (err) {
+      res.status(500).send('Error occurred: database error');
+    } else {
+      res.send(comment);
+    }
+  });
+});
+
+module.exports = router;
